bde855f0edf2dd96240a5c1a1dfd028bc92fd38d
Nihey Takizawa <nihey.takizawa@gmail.com>
2017-05-04 00:57:45 -0300

    Fix extractCSS loader sentence

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        use: extractCSS.extract({
           use: ['css-loader'],
         }),
       },
